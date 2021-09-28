92312b5142530162e0ec2b52aee35a88e79c8bcb
kiwoon kim <kisoon0947@gmail.com>
2021-09-28 17:01:26 +0900

    update

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@
     let currentScene = 0 // 현재 활성하된(눈 앞에 보고 있는 ) 화면
     let enterNewScene = false // 새로운 scene이 시작되는 순간 false
 
+
+
     const sceneInfo = 
     [
         {   
@@ -23,8 +25,11 @@
             },
             values:
             {
-                messageA_opacity: [ 0, 1, { start: 0.1 , end: 0.2 }],
-                messageB_opacity: [ 0, 1, { start: 0.3 , end: 0.4 }],
+                messageA_opacity_in: [ 0, 1, { start: 0.1 , end: 0.2 }],
+                messageA_translateY_in: [ 20, 0, { start: 0.1 , end: 0.2 }],
+                messageB_opacity_in: [ 0, 1, { start: 0.3 , end: 0.4 }],
+                messageA_opacity_out: [ 1, 0, { start: 0.25 , end: 0.3 }],
+                messageA_translateY_out: [ 0, -20, { start: 0.25 , end: 0.3 }],
 
             }
 
@@ -62,6 +67,9 @@
 
     ]
 
+
+
+
     const setLayout = () =>
     {
         // 각 스크롤 섹션의 높이 세팅
@@ -86,6 +94,9 @@
         document.body.setAttribute('id', `show-scene-${currentScene}`)
          
     }
+
+
+
 
     const calcValues = ( values, currentYOffset ) =>
     {
@@ -122,17 +133,36 @@
         return rv
     }
 
+
+
+
     const playAnimation = () =>
     {   
         const objects = sceneInfo[currentScene].objects
         const values = sceneInfo[currentScene].values
         const currentYOffset = yOffset - prevScrollHeight
+        const scrollHeight = sceneInfo[currentScene].scrollHeight
+        const scrollRatio = ( yOffset - prevScrollHeight) / scrollHeight
 
         switch ( currentScene )
         {
             case 0:
-                let messageA_opacity_in = calcValues(values.messageA_opacity , currentYOffset)
-                objects.messageA.style.opacity = messageA_opacity_in
+                const messageA_opacity_in = calcValues(values.messageA_opacity_in , currentYOffset)
+                const messageA_opacity_out = calcValues(values.messageA_opacity_out , currentYOffset)
+                const messageA_translateY_in = calcValues(values.messageA_translateY_in , currentYOffset)
+                const messageA_translateY_out = calcValues(values.messageA_translateY_out , currentYOffset)
+
+                if( scrollRatio <= 0.22)
+                {
+                    objects.messageA.style.opacity = messageA_opacity_in
+                    objects.messageA.style.transform = `translateY(${messageA_translateY_in}%)`
+                } 
+                else
+                {
+                    objects.messageA.style.opacity = messageA_opacity_out
+                    objects.messageA.style.transform = `translateY(${messageA_translateY_out}%)`
+                }
+                
                 break    
             case 1:
                 break    
@@ -144,6 +174,8 @@
     }
 
         
+
+
     const scrollLoop = () =>
     {   
         enterNewScene = false
@@ -168,12 +200,13 @@
             }
 
             if( enterNewScene ) return       
-            console.log(currentScene)
             playAnimation()     
     }
         
         
         
+
+
         
     window.addEventListener('scroll',() =>
     {
